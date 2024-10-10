d23414eb035cea71afc43675df737e6f864877a8
William Song <wihsong@ucsc.edu>
2024-10-10 00:27:23 -0700

    not needed

--- resources/asgn4/test_scripts/nconflict_pause.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source_dir=`dirname ${BASH_SOURCE}`
-
-workload=$source_dir/../workloads/nconflict_pause.toml
-nthreads=4
-
-$source_dir/test_workload.sh $workload $nthreads
-exit $?
